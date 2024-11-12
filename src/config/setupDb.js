b5951a65302d9e96ca449e6a84224d9c652c9af3
Javier Jimenez <javier.sniguel@gmail.com>
2024-11-12 10:36:07 -0300

    se subio la parte del back dle proyecto

---
+++ src/config/setupDb.js
@@ -0,0 +1,40 @@
+const { Sequelize } = require('sequelize');
+
+let seqInstance = null;
+
+
+
+const createInstance = async () => {
+    const instance = new Sequelize(
+        'studentsdb', // nombre de base de datos
+        'root', // usuario
+        'feoso123', // contraseña
+        {
+            host: 'localhost',
+            dialect: 'mysql',
+            pool: {
+                max: 3
+            }
+        }
+    );
+
+    try {
+        await instance.authenticate();
+        console.log('Connection has been established successfully.');
+        return instance;
+    } catch (error) {
+        throw new Error('Unable to connecto to database');
+    }
+};
+
+const getSeqInstance = async () => {
+    if (!seqInstance) {
+        seqInstance = await createInstance();
+    }
+
+    return seqInstance;
+};
+
+module.exports = {
+    getSeqInstance
+};
